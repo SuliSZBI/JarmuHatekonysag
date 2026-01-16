72fb9d3fffcd4c7b43cd3507303f2217077b714d
boto82 <smurfnek2@gmail.com>
2026-01-16 13:13:22 +0100

    valtozas

---
+++ frontend/public/kocsik.js
@@ -0,0 +1,44 @@
+let kocsik = [
+    {
+        tipus: 'Honda Civic',
+        motortipus: '1.5 VTEC Turbo benzines',
+        fogyasztas: '6.5 l/100km',
+        evjarat: 2020,
+        teljesitmenyLE: 182,
+        valto: 'manuális',
+    },
+    {
+        tipus: 'Toyota Corolla',
+        motortipus: '1.8 Hybrid',
+        fogyasztas: '3.8 l/100km',
+        evjarat: 2021,
+        teljesitmenyLE: 122,
+        valto: 'automata (CVT)',
+    },
+    {
+        tipus: 'Volkswagen Golf',
+        motortipus: '2.0 TDI dízel',
+        fogyasztas: '4.5 l/100km',
+        evjarat: 2019,
+        teljesitmenyLE: 150,
+        valto: 'manuális',
+    },
+    {
+        tipus: 'Ford Focus',
+        motortipus: '1.0 EcoBoost benzines',
+        fogyasztas: '5.9 l/100km',
+        evjarat: 2018,
+        teljesitmenyLE: 125,
+        valto: 'manuális',
+    },
+    {
+        tipus: 'BMW 320d',
+        motortipus: '2.0 dízel',
+        fogyasztas: '4.7 l/100km',
+        evjarat: 2022,
+        teljesitmenyLE: 190,
+        valto: 'automata',
+    }
+];
+
+export default kocsik;
--- frontend/src/pages/GeoLocation.jsx
+++ frontend/src/pages/GeoLocation.jsx
@@ -1,14 +1,71 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Geolocation.css";
+import kocsik from "../../public/kocsik";
 
 const GeoLocation = () => {
   const [longitude, setLongitude] = useState("");
+  const [altitude, setAltitude] = useState(0);
   const [latitude, setLatitude] = useState("");
+  const [kocsi, setKocsi] = useState("");
+  const [kapKoordinata, setKapKoordinata] = useState(1);
 
+  useEffect(() => {
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+      } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+
+    function success(position) {
+      console.log(position.coords.longitude);
+      console.log(position.coords.latitude);
+      console.log(position.coords.altitude);
+      
+      setLongitude(position.coords.longitude);
+      setLatitude(position.coords.latitude);
+      setAltitude(position.coords.altitude);
+    }
+
+    function error() {
+      setKapKoordinata(0);
+    }
+
+    getLocation();
+
+  }, []);
+
+  const bekuldes = (e) => {
+    e.preventDefault(); 
+  }
+  
   return (
     <div className="geolocation-container">
       <h1>Geolocation</h1>
       <p>Egy űrlap, ami bekéri a koordinátákat ...</p>
+      <form>
+        <select id="kocsik" onChange={e => setKocsi(e.target.value)}>
+          {kocsik.map((elem, index) => (
+            <option key={index} value={elem.tipus}>{elem.tipus}</option>
+          ))}
+        </select>
+        {kapKoordinata ?
+          <div>
+            <p>Vízszintes koordináta: {longitude}</p>
+            <p>Függőleges koordináta: {latitude}</p>
+            <p>Kiindulási magasság: {altitude}</p>
+          </div>
+        :
+          <>
+            <label>Add meg a függőleges koordinát.</label>
+            <input onChange={e => setLatitude(e.target.value)} id="latitudeKoord" type="text" />
+            <label>Add meg a vízszintes koordinát.</label>
+            <input onChange={e => setLongitude(e.target.value)} id="longitudeKoord" type="text" />
+          </>
+        }
+        <button onClick={e => bekuldes(e)}>Beküldés</button>
+      </form>
     </div>
   );
 };
